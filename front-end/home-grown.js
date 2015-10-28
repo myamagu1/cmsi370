ddb45d6f9aff1cf8f55230a186ec83c86b5da747
Mondo <myamagu1@lmu.lion.edu>
2015-10-28 16:46:33 -0700

    made some minor changes

--- front-end/home-grown.js
+++ front-end/home-grown.js
@@ -2,9 +2,14 @@ $(function () {
 
 	$("#search-button").click(function () {
 		$.getJSON(
-				//URL
-				//parameters
-			);
+    		"http://lmu-diabolical.appspot.com/characters",
+    		function (characters) {
+        		// Do something with the character list.
+        		characters.forEach(function (character) {
+            		console.log(character);
+        		});
+    		}
+		);
 	});
 
 });
