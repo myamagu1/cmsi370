1e27bace386ed83782928921b701fcd651a2c2e6
Mondo <myamagu1@lmu.lion.edu>
2015-11-01 14:49:28 -0800

    some progress on the listing characters portion

--- front-end/home-grown.js
+++ front-end/home-grown.js
@@ -6,30 +6,10 @@ $(function () {
     		function (characters) {
         		// Do something with the character list.
         		characters.forEach(function (character) {
-        			document.write(character.id, character.classType);
-        			document.write("<br>");
+        			document.write("&middot " + "Character ID: " + character.id + " ClassType: " + character.classType + 
+        				"  Gender: " + character.gender);
+        			document.write("<br>" + "<br>");
         		});
-
-        		var linebreak = "<br />"
-var my_var = "Hello World!"
-
-document.write(my_var)
-document.write(linebreak)
-
-my_var = "I am learning JavaScript!"
-document.write(my_var)
-document.write(linebreak)
-
-my_var = "Script is Finishing up..."
-document.write(my_var)
-				/*var items = [];
-				$.each( characters, function( key, val ) {
-				    items.push( "<li id='" + key + "'>" + val + "</li>" );
-				  });
-				  $( "<ul/>", {
-				    "class": "my-new-list",
-				    html: items.join( "" )
-				  }).appendTo( "body" );*/
     		}
 		);
 	});
