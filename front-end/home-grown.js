a2faeb2a271372794c98442e2789811e980ebc4d
Mondo <myamagu1@lmu.lion.edu>
2015-11-01 17:01:16 -0800

    made changes on the listing characters part

--- front-end/home-grown.js
+++ front-end/home-grown.js
@@ -1,3 +1,22 @@
+/*$(function () {
+	$("#search-button").click(function () {
+		$.getJSON(
+    		"http://lmu-diabolical.appspot.com/characters",
+    		function (characters) {
+        		// Do something with the character list.
+        		characters.forEach(function (character) {
+        			document.write("&middot" + " Character ID: " + character.id + " ClassType: " + character.classType + 
+        				" Gender: " + character.gender);
+        			document.write("<br/>" + "<br/>");
+        		});
+    		}
+		);
+	});
+
+});
+*/
+
+
 $(function () {
 
 	$("#search-button").click(function () {
@@ -6,9 +25,8 @@ $(function () {
     		function (characters) {
         		// Do something with the character list.
         		characters.forEach(function (character) {
-        			document.write("&middot " + "Character ID: " + character.id + " ClassType: " + character.classType + 
-        				"  Gender: " + character.gender);
-        			document.write("<br>" + "<br>");
+    			    $("body").append($("<div></div>").text(" Character ID: " + character.id + " ClassType: " + character.classType +
+                                    " Gender: " + character.gender));
         		});
     		}
 		);
