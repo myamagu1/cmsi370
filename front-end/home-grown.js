3217fbc8f6694ace251fcfcb2b1b33829f4c6acd
Mondo <myamagu1@lmu.lion.edu>
2015-11-03 20:39:40 -0800

    now i can search and modify a character

--- front-end/home-grown.js
+++ front-end/home-grown.js
@@ -23,7 +23,7 @@ $(function () {
                 data: JSON.stringify({
                     name: $("#name").val(),
                     classType: $("#classType").val(),
-                    gender: "MALE",
+                    gender: "MALE",  // $("#gender").val(),
                     level: $("#level").val(),
                     money: $("#money").val()
                 }),
@@ -36,6 +36,37 @@ $(function () {
                         jqXHR.getResponseHeader("Location"));
                 }
             });
+        });
+    });
+
+    $("#search-button").click(function () {
+        $.getJSON(
+            "http://lmu-diabolical.appspot.com/characters/" + $("#searchCharacter").val(),
+            function (character) {
+                // Do something with the character.
+                console.log(character);
+            }
+        );
+    });
+
+    $("#modify-button").click(function () {
+        $.ajax({
+            type: 'PUT',
+            url: "http://lmu-diabolical.appspot.com/characters/" + $("#characterId2").val(),
+            data: JSON.stringify({
+                id: $("#characterId2").val(),
+                name: $("#name2").val(),
+                classType: $("#classType2").val(),
+                gender: "MALE", //$("#gender2").val(),
+                level: $("#level2").val(),
+                money: $("#money2").val()
+            }),
+            contentType: "application/json",
+            dataType: "json",
+            accept: "application/json",
+            success: function (data, textStatus, jqXHR) {
+                console.log("Done: no news is good news.");
+            }
         });
     });
 });
