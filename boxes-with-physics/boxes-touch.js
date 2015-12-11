b76f75523fec33273fb2ed62674eef6dda9daef8
Mondo <myamagu1@lmu.lion.edu>
2015-12-10 22:10:32 -0800

    some progress

--- boxes-with-physics/boxes-touch.js
+++ boxes-with-physics/boxes-touch.js
@@ -3,16 +3,88 @@
      * Tracks a box as it is rubberbanded or moved across the drawing area.
      */
     var trackDrag = function (event) {
-        $.each(event.changedTouches, function (index, touch) {
-            // Don't bother if we aren't tracking anything.
-            if (touch.target.movingBox) {
-                // Reposition the object.
-                touch.target.movingBox.offset({
-                    left: touch.pageX - touch.target.deltaX,
-                    top: touch.pageY - touch.target.deltaY
-                });
+    $.each(event.changedTouches, function (index, touch) {
+        event.preventDefault();
+        // Don't bother if we aren't tracking anything.
+        if (touch.target.movingBox) {
+            var boxParent = $(touch.target.movingBox).parent(),
+                parentWidth = boxParent.width(),
+                parentHeight = boxParent.height();
+                parentLeft = boxParent.offset().left,
+                parentTop = boxParent.offset().top,
+                parentRight = parentLeft + parentWidth,
+                parentBottom = parentTop + parentHeight;
+
+            // Reposition the object.
+            touch.target.movingBox.offset ({
+                left: touch.pageX - touch.target.deltaX,
+                top: touch.pageY - touch.target.deltaY
+            });
+
+            //  Adds Deletion Highlight
+            // JD: Nice non-hardcoded drawing area :)
+            if (touch.pageX - touch.target.deltaX > parentRight || touch.pageY - touch.target.deltaY > parentBottom || 
+                touch.pageX - touch.target.deltaX < parentLeft || touch.pageY - touch.target.deltaY < parentTop) {
+                touch.target.movingBox.addClass("box-delete deletion-highlight");
+            };
+            
+            /* Removes Deletion Highlight,
+             * used if() instead of else() due to on-move oscillation between
+             * box-highlight and deletion-highlight with else() 
+             */
+            if (touch.pageX - touch.target.deltaX <= parentRight && touch.pageY - touch.target.deltaY <= parentBottom && 
+                touch.pageX - touch.target.deltaX >= parentLeft && touch.pageY - touch.target.deltaY >= parentTop) {
+                touch.target.movingBox.removeClass("box-delete deletion-highlight");
             }
-        });
+        }
+
+        var cacheEntry = cache[touch.identifier];
+        if (cacheEntry && cacheEntry.creation) {
+            var createLeft, createTop, createWidth, createHeight;
+
+            if (touch.pageX < cacheEntry.initialX) {
+                createLeft = touch.pageX;
+                createWidth = cacheEntry.initialX - touch.pageX;
+                // JD: Some code duplication with the Y computations.  Pull out to a function maybe?
+                if (touch.pageY < cacheEntry.initialY) {
+                    createTop = touch.pageY;
+                    createHeight = cacheEntry.initialY - touch.pageY;
+                } else {
+                    createTop = cacheEntry.initialY;
+                    createHeight = touch.pageY - cacheEntry.initialY;
+                }
+            } else {
+                createLeft = cacheEntry.initialX;
+                createWidth = touch.pageX - cacheEntry.initialX;
+                if (touch.pageY < cacheEntry.initialY) {
+                    createTop = touch.pageY;
+                    createHeight = cacheEntry.initialY - touch.pageY;
+                } else {
+                    createTop = cacheEntry.initialY;
+                    createHeight = touch.pageY - cacheEntry.initialY;
+                }
+            }
+
+            cacheEntry.creation
+                .offset({
+                    left: createLeft,
+                    top: createTop
+                })
+                .width(createWidth)
+                .height(createHeight);
+        }
+    });
+    // var trackDrag = function (event) {
+    //     $.each(event.changedTouches, function (index, touch) {
+    //         // Don't bother if we aren't tracking anything.
+    //         if (touch.target.movingBox) {
+    //             // Reposition the object.
+    //             touch.target.movingBox.offset({
+    //                 left: touch.pageX - touch.target.deltaX,
+    //                 top: touch.pageY - touch.target.deltaY
+    //             });
+    //         }
+    //     });
 
         // Don't do any touch scrolling.
         event.preventDefault();
@@ -28,29 +100,53 @@
                 // touch.target.movingBox.
                 touch.target.movingBox = null;
             }
-        });
-    };
+
+            var cacheEntry = cache[touch.identifier];
+                if (cacheEntry && cacheEntry.creation) {
+                    // Do we want to keep it?
+                    var $creation = $(cacheEntry.creation);
+                    // JD: Yay, minimum size check!  To give "20" this meaning, it would probably
+                    //     be good to stick it as another "private" variable like:
+                    //
+                    //     var MINIMUM_WIDTH = 20, MINIMUM_HEIGHT = 20;
+                    if ($creation.width() < 20 || $creation.height() < 20) {
+                        $creation.remove();
+                    }
+
+                    cacheEntry.creation.removeClass("creation-highlight");
+                    cacheEntry.creation = null;
+                    // Clean up.
+                    delete cache[touch.identifier];
+                }
+            });
+        };
+    //     });
+    // };
 
     /**
      * Indicates that an element is unhighlighted.
      */
     var unhighlight = function () {
         $(this).removeClass("box-highlight");
+        if ($(this).hasClass("box-delete")) {
+            $(this).remove();
+        }
     };
+    // var unhighlight = function () {
+    //     $(this).removeClass("box-highlight");
+    // };
 
     /**
      * Begins a box move sequence.
      */
     var startMove = function (event) {
-        $("#console").text("Inside startMove");
         $.each(event.changedTouches, function (index, touch) {
-            $("#console").text("Processing touch " + touch.id);
-            // Highlight the element.
-            $(touch.target).addClass("box-highlight");
+        // Highlight the element.
+        $(touch.target).addClass("box-highlight");
 
-            // Take note of the box's current (global) location.
-            var jThis = $(touch.target),
-                startOffset = jThis.offset();
+        // Take note of the box's current (global) location.
+        var jThis = $(touch.target),
+            startOffset = jThis.offset();
 
             // Set the drawing area's state to indicate that it is
             // in the middle of a move.
@@ -63,6 +159,28 @@
         // deal with it.
         event.stopPropagation();
     };
+    // var startMove = function (event) {
+    //     $("#console").text("Inside startMove");
+    //     $.each(event.changedTouches, function (index, touch) {
+    //         $("#console").text("Processing touch " + touch.id);
+    //         // Highlight the element.
+    //         $(touch.target).addClass("box-highlight");
+
+    //         // Take note of the box's current (global) location.
+    //         var jThis = $(touch.target),
+    //             startOffset = jThis.offset();
+
+    //         // Set the drawing area's state to indicate that it is
+    //         // in the middle of a move.
+    //         touch.target.movingBox = jThis;
+    //         touch.target.deltaX = touch.pageX - startOffset.left;
+    //         touch.target.deltaY = touch.pageY - startOffset.top;
+    //     });
+
+    //     // Eat up the event so that the drawing area does not
+    //     // deal with it.
+    //     event.stopPropagation();
+    // };
 
     /**
      * Sets up the given jQuery collection as the drawing area(s).
@@ -75,6 +193,8 @@
             // Event handler setup must be low-level because jQuery
             // doesn't relay touch-specific event properties.
             .each(function (index, element) {
+                //
+                element.addEventListener("touchstart", startCreate, false);
                 element.addEventListener("touchmove", trackDrag, false);
                 element.addEventListener("touchend", endDrag, false);
             })
@@ -82,28 +202,51 @@
             .find("div.box").each(function (index, element) {
                 element.addEventListener("touchstart", startMove, false);
                 element.addEventListener("touchend", unhighlight, false);
-                element.velocity = { x: 0, y: 0 };
-                element.acceleration = { x: 0, y: 0 };
+                // element.velocity = { x: 0, y: 0 };
+                // element.acceleration = { x: 0, y: 0 };
             });
     };
+    
+    // 
+    var startCreate = function (event) {
+        $.each(event.changedTouches, function(index, touch) {
+            cacheEntry = { };
 
-    var lastTimeStamp = 0;
-    var FRAME_RATE = 10;
-    var MS_BETWEEN_FRAMES = 1000 / FRAME_RATE;
-    var updateBoxes = function (timestamp) {
-        var timePassed = timestamp - lastTimeStamp;
-        if (timePassed > MS_BETWEEN_FRAMES) {
-            $("div.box").each(function (index, element) {
-                var offset = $(element).offset();
-                offset.left += element.velocity.x * timePassed;
-                offset.top += element.velocity.y * timePassed;
+            cacheEntry.initialX = touch.pageX;
+            cacheEntry.initialY = touch.pageY;
 
-                element.velocity.x += element.acceleration.x * timePassed;
-                element.velocity.y += element.acceleration.y * timePassed;
-                $(element).offset(offset);
+            var createdBox = '<div class="box" style="width: 0px; height: 0px; height: 0px; left:' 
+            + touch.pageX + 'px; top: ' + touch.pageY + 'px">' + '</div>';
+
+            $("#drawing-area").append(createdBox);
+            cacheEntry.creation =  $("div div:last-child");
+            cacheEntry.creation.addClass("creation-highlight");
+
+            $("#drawing-area").find("div.box").each(function(index, element) {
+                    element.addEventListener("touchstart", BoxesTouch.startMove, false);
+                    element.addEventListener("touchend", BoxesTouch.unhighlight, false);
             });
+        });
+        event.stopPropagation();
+    };
 
-            lastTimeStamp = timestamp;
+    // var lastTimeStamp = 0;
+    // var FRAME_RATE = 10;
+    // var MS_BETWEEN_FRAMES = 1000 / FRAME_RATE;
+    // var updateBoxes = function (timestamp) {
+    //     var timePassed = timestamp - lastTimeStamp;
+    //     if (timePassed > MS_BETWEEN_FRAMES) {
+    //         $("div.box").each(function (index, element) {
+    //             var offset = $(element).offset();
+    //             offset.left += element.velocity.x * timePassed;
+    //             offset.top += element.velocity.y * timePassed;
+
+    //             element.velocity.x += element.acceleration.x * timePassed;
+    //             element.velocity.y += element.acceleration.y * timePassed;
+    //             $(element).offset(offset);
+    //         });
+
+    //         lastTimeStamp = timestamp;
     //         $("#timestamp").text(delta);
 
     //         // Move the boxes... on. Their. Own!!!
