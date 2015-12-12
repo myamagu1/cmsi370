a397004be7ea7cd27bc6e29848310dfb406a2f72
Mondo <myamagu1@lmu.lion.edu>
2015-12-11 21:04:27 -0800

    flicking doesn't work but the boxes are now blue

--- boxes-with-physics/boxes-touch.js
+++ boxes-with-physics/boxes-touch.js
@@ -157,8 +157,8 @@
                 "y: " + event.accelerationIncludingGravity.y);
 
             $("div.box").each(function (index, element) {
-                element.acceleration.x = event.accelerationIncludingGravity.x /1000000;
-                element.acceleration.y = -event.accelerationIncludingGravity.y /1000000;
+                element.acceleration.x = event.accelerationIncludingGravity.x /100000;
+                element.acceleration.y = -event.accelerationIncludingGravity.y /100000;
             });
         });
     };
